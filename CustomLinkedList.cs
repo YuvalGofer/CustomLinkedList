9be9b0e78221c90e2ab35b83fe7db9970167e700
yuval gofer <yuvyael@gmail.com>
2022-03-23 11:27:04 +0200

    Add method and foreach. still having issue with IEnumerableAdding Add method.

---
+++ CustomLinkedList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedList
+{
+     class CustomLinkedList<T> 
+    {
+        public Node<T> First { get; set; }
+        public Node<T> Current { get; set; }
+        public Node<T> Last { get; set; }
+       
+        public void Add(T value)
+        {
+            Node<T> newNode = new Node<T>(value);
+            if (Current == null)
+            {
+                First = newNode;
+                Current = newNode;
+            }
+            else
+            {
+                Current.Next = newNode;
+                newNode.Previous = Current;
+                Current = newNode;  
+            }
+            Last = newNode;
+            
+        }
+        public IEnumerable<T> GetEnumerableDESC(T item)
+        {
+            Current = Last;
+            while(Current != null)
+            {
+                yield return Current.Value;
+                Current = Current.Previous;
+            }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = First;
+            while(current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+        //IEnumerator IEnumerable.GetEnumerator()
+        //{
+        //    Current = First;
+        //    while(Current != null)
+        //    {
+        //        if(Current.Value == null)
+        //            yield break;
+        //    }
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomLinkedList<int> cll = new CustomLinkedList<int>();
+            cll.Add(1);
+            cll.Add(2);
+            cll.Add(3);
+            Console.WriteLine("Enumarating CustomLinkedList items with");
+            Console.WriteLine("GetEnumerator:");
+            foreach (var item in cll)
+                Console.WriteLine(item);
+            //
+            Console.WriteLine("GetEnumerableDESC:");
+            foreach (var item in cll.GetEnumerableDESC())
+                Console.WriteLine(item);
+ 
+        }          
+    }
+}
